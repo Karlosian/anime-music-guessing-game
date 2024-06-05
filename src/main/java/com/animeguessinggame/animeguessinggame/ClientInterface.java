edc23e0b9eacc6290735ef30d46e1f167a5608d3
Dinh Viet Luong <dinhvietluong99@gmail.com>
2024-06-05 11:53:27 -0400

    started UI elements

---
+++ src/main/java/com/animeguessinggame/animeguessinggame/ClientInterface.java
@@ -0,0 +1,90 @@
+package com.animeguessinggame.animeguessinggame;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+import javafx.util.Duration;
+import org.controlsfx.control.textfield.TextFields;
+
+import uk.co.caprica.vlcj.factory.MediaPlayerFactory;
+import uk.co.caprica.vlcj.factory.discovery.NativeDiscovery;
+import uk.co.caprica.vlcj.javafx.videosurface.ImageViewVideoSurface;
+import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.animeguessinggame.animeguessinggame.GameApplication.window;
+
+public class ClientInterface {
+    public static List<String> possibleSuggestions;
+    private TextField answerBox;
+
+    private ProgressBar progressBar;
+    private Timeline timer;
+    private double remainingTime;
+
+    private ImageView imageView;
+    private EmbeddedMediaPlayer mediaPlayer;
+    private MediaPlayerFactory mediaPlayerFactory = new MediaPlayerFactory();
+
+    private Scene scene;
+
+    public void display() {
+        window.setScene(scene);
+        startVideo("https://v.animethemes.moe/Naruto-OP1.webm");
+        startTimer();
+    }
+
+    public ClientInterface() throws IOException {
+        new NativeDiscovery().discover();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("game-view.fxml"));
+        Parent root = fxmlLoader.load();
+
+        scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+
+        // Auto-completion setup
+        answerBox = (TextField) root.lookup("#answerBox");
+
+        possibleSuggestions = Arrays.asList(
+                "C", "C#", "C++", "F#", "GoLang",
+                "Dart", "Java", "JavaScript", "Kotlin", "PHP",
+                "Python", "R", "Swift", "Visual Basic .NET"
+        );
+
+        TextFields.bindAutoCompletion(answerBox, possibleSuggestions);
+
+        imageView = (ImageView) root.lookup("#imageView");
+        progressBar = (ProgressBar) root.lookup("#timeLeft");
+    }
+
+    public void startVideo(String url) {
+        // VLCJ setup
+        mediaPlayer = mediaPlayerFactory.mediaPlayers().newEmbeddedMediaPlayer();
+        mediaPlayer.videoSurface().set(new ImageViewVideoSurface(imageView));
+        mediaPlayer.media().play(url);
+    }
+
+    private void startTimer() {
+        remainingTime = 40;
+        timer = new Timeline(new KeyFrame(Duration.seconds(0.01), event -> {
+            remainingTime -= 0.01;
+            progressBar.setProgress(remainingTime / 30);
+
+            if (remainingTime <= 0) {
+                timer.stop();
+            }
+        }));
+        timer.setCycleCount(Timeline.INDEFINITE);
+        timer.play();
+    }
+}
--- src/main/java/com/animeguessinggame/animeguessinggame/GameApplication.java
+++ src/main/java/com/animeguessinggame/animeguessinggame/GameApplication.java
@@ -4,64 +4,47 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.Hyperlink;
 import javafx.stage.Stage;
-import javafx.embed.swing.SwingNode;
-import org.controlsfx.control.textfield.TextFields;
-import uk.co.caprica.vlcj.factory.MediaPlayerFactory;
-import uk.co.caprica.vlcj.factory.discovery.NativeDiscovery;
-import uk.co.caprica.vlcj.player.base.MediaPlayer;
-import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
 
-import uk.co.caprica.vlcj.javafx.videosurface.ImageViewVideoSurface;
-import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
-
-import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class GameApplication extends Application {
-    private EmbeddedMediaPlayer mediaPlayer;
-    private MediaPlayerFactory mediaPlayerFactory = new MediaPlayerFactory();
+    public static Stage window;
+    private static Scene scene;
+    private static ClientInterface clientInterface;
 
     @Override
     public void start(Stage stage) throws IOException {
-        new NativeDiscovery().discover();
-
-        FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("game-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("home-page.fxml"));
         Parent root = fxmlLoader.load();
 
-        Scene scene = new Scene(root, 800, 600);
-        stage.setResizable(false);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        scene.getStylesheets().add(this.getClass().getResource("style.css").toExternalForm());
+        scene = new Scene(root);
 
-        // Auto-completion setup
-        TextField answerBox = (TextField) root.lookup("#answerBox");
+        Hyperlink link = (Hyperlink) root.lookup("#downloadLink");
+        link.setOnAction(event -> {
+            try {
+                Desktop.getDesktop().browse(new URI("https://www.videolan.org/vlc/"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
+        });
 
-        List<String> possibleSuggestions = Arrays.asList(
-                "C", "C#", "C++", "F#", "GoLang",
-                "Dart", "Java", "JavaScript", "Kotlin", "PHP",
-                "Python", "R", "Swift", "Visual Basic .NET"
-        );
+        window = stage;
+        window.setScene(scene);
+        window.show();
 
-        TextFields.bindAutoCompletion(answerBox, possibleSuggestions);
-
-        // VLCJ setup
-        String webmUrl = "https://v.animethemes.moe/DragonBall-OP1.webm";
-        mediaPlayer = mediaPlayerFactory.mediaPlayers().newEmbeddedMediaPlayer();
-        ImageView imageView = (ImageView) root.lookup("#imageView");
-        mediaPlayer.videoSurface().set(new ImageViewVideoSurface(imageView));
-
-        stage.show();
-
-        mediaPlayer.media().play(webmUrl);
+        clientInterface = new ClientInterface();
     }
 
+    public void startGame() {
+        clientInterface.display();
+    }
 
     @Override
     public void stop() throws Exception {
