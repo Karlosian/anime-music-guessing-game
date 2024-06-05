937c45c1614e24120b97ca9b68cbb3a54f04c4dc
Dinh Viet Luong <dinhvietluong99@gmail.com>
2024-06-05 14:01:06 -0400

    homepage implementation

--- src/main/java/com/animeguessinggame/animeguessinggame/GameApplication.java
+++ src/main/java/com/animeguessinggame/animeguessinggame/GameApplication.java
@@ -1,16 +1,18 @@
 package com.animeguessinggame.animeguessinggame;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.awt.*;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.net.*;
+import java.util.Enumeration;
 
 public class GameApplication extends Application {
     public static Stage window;
@@ -44,6 +46,53 @@ public class GameApplication extends Application {
 
     public void startGame() {
         clientInterface.display();
+    }
+
+    public void createRoom() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("create-room.fxml"));
+        Parent root = fxmlLoader.load();
+
+        Label roomCode = (Label) root.lookup("#roomCode");
+        try {
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+            while (interfaces.hasMoreElements()) {
+                NetworkInterface iface = interfaces.nextElement();
+                // filters out 127.0.0.1 and inactive interfaces
+                if (iface.isLoopback() || !iface.isUp())
+                    continue;
+
+                Enumeration<InetAddress> addresses = iface.getInetAddresses();
+                while(addresses.hasMoreElements()) {
+                    InetAddress addr = addresses.nextElement();
+                    roomCode.setText(addr.getHostAddress());
+                    // System.out.println(iface.getDisplayName() + " " + ip);
+                }
+            }
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        }
+
+        Hyperlink link = (Hyperlink) root.lookup("#linkToMAL");
+        link.setOnAction(event -> {
+            try {
+                Desktop.getDesktop().browse(new URI("https://myanimelist.net/apiconfig"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        scene = new Scene(root);
+        window.setScene(scene);
+    }
+
+    public void joinRoom() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("join-room.fxml"));
+        Parent root = fxmlLoader.load();
+
+        scene = new Scene(root);
+        window.setScene(scene);
     }
 
     @Override
