19ad138228e7c4e277e75a33e69ad2e9cc32c471
Karlosian <kjmiles360@gmail.com>
2024-06-05 23:23:02 -0400

    some server implementation progress

--- src/main/java/com/animeguessinggame/animeguessinggame/JoinRoomController.java
+++ src/main/java/com/animeguessinggame/animeguessinggame/JoinRoomController.java
@@ -1,8 +1,14 @@
 package com.animeguessinggame.animeguessinggame;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 import static java.lang.Integer.parseInt;
 
@@ -25,9 +31,17 @@ public class JoinRoomController {
         roomIP = RoomCodeField.getText();
         roomPort = parseInt(RoomPortField.getText());
         GameClient.connectClient(roomIP,roomPort,MALusername);
+        // Load the waiting-room.fxml
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("waiting-room.fxml"));
+            Parent waitingRoomRoot = loader.load();
+            Scene waitingRoomScene = new Scene(waitingRoomRoot);
+
+            // Get the current stage
+            Stage stage = (Stage) RoomCodeField.getScene().getWindow();
+            stage.setScene(waitingRoomScene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
-
-
-
-}
+    }
